5efbea622ac2c9f6183a8c2402c1b83d00d99ce9
TeddyGavi <matc.davis@gmail.com>
2022-10-26 14:48:49 -0700

    update dayList to use prop function passed down from parent

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,23 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
+export default function DayList(props) {
+
+  const days = props.days.map(x => {
+    return (
+      <DayListItem 
+        key={x.id}
+        name={x.name}
+        spots={x.spots}
+        selected={x.name === props.day}
+        setDay={props.setDay}
+        />
+    )
+  })
+
   return (
     <ul>
-      
+      {days}
     </ul>
   )
 }

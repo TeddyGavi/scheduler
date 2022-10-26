3397429b83da0202d8e36b9186215b7ea2cf9f4c
TeddyGavi <matc.davis@gmail.com>
2022-10-26 12:25:27 -0700

    rafactor button.js to use classnames to build className

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
+import classNames from "classnames";
 
-import "components/Button.scss";
+import "../components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+  
+
+   return (
+      <button
+         /* same thing as using spread {...props} */
+         disabled={props.disabled}
+         onClick={props.onClick}
+         /******************************************/
+
+         className={buttonClass}
+      >
+         {props.children}
+      </button>
+      )
 }

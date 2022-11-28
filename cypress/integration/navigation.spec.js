5dbf437d5ac32ad9c957d76656b754999d0a0236
TeddyGavi <matc.davis@gmail.com>
2022-11-28 11:10:15 -0800

    add basic cypress tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,17 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to tuesday", () => {
+    cy.visit("/");
+    /*    cy.get('li.day-list__item')
+      .should('contain', 'Tuesday')
+    cy.contains("li", 'Tuesday')
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)") */
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
